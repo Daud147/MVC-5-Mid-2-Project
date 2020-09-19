4f269806e7839ddbd156bd67a009957902f8b257
Daud Sikander <62802356+Daud147@users.noreply.github.com>
2020-09-19 23:44:42 +0500

    Mid 2 Project

---
+++ MvcLibrary_Daud_Sikander/MvcLibrary_Daud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcLibrary_Daud.MvcApplication" Language="C#" %>
---
+++ MvcLibrary_Daud_Sikander/MvcLibrary_Daud/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcLibrary_Daud.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
